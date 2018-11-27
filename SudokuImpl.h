419e7e0fbc73edea0bca8a7b84fc4422c7c4e966
alisoleimani1999 <40151904+alisoleimani1999@users.noreply.github.com>
2018-11-27 08:00:13 -0500

    Create SudokuImpl.h

---
+++ SudokuImpl.h
@@ -0,0 +1,22 @@
+
+#ifndef PROYECTOSUDOKU_FINAL_SUDOKUIMPL_H
+#define PROYECTOSUDOKU_FINAL_SUDOKUIMPL_H
+
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+class  SudokuImpl {
+private:
+    int ** matrix;
+    int size;
+    void fill_random_init(int num_hints);
+public:
+    SudokuImpl(int size, int num_hints);
+    ~SudokuImpl();
+    bool is_done();
+    bool eval_insert(int row, int col, int eval_num);
+    void insert_num(int row, int col, int num);
+    void print();
+};
